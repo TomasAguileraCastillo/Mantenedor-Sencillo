3a813c7b66ba916e806a20bc6e1782af8089acc4
Tomás Aguilera <136927556+TomasAguileraCastillo@users.noreply.github.com>
2024-11-22 20:09:24 -0300

    Update Conexion.php

--- mantenedor/config/Conexion.php
+++ mantenedor/config/Conexion.php
@@ -7,7 +7,7 @@ require_once "global.php";
 $conexion = new mysqli(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
 
 //consulta a la base de datos con la codificacion de caracteres 
-mysqli_query( $conexion, 'SET NAMES "'.DB_ENCODE.'"');
+mysqli_query( $conexion, 'SET NAMES "'.DB_ENCODE.'"') ;
 
 
 
